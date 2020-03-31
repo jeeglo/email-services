7309de18caded73a9c3b34273823107fb08f08a5
Imran Siddiq <imransiddiq91@gmail.com>
2020-03-31 17:03:37 +0500

    New Autoresponder added: EmailOctopus

--- src/EmailService.php
+++ src/EmailService.php
@@ -13,6 +13,8 @@ use Jeeglo\EmailService\Drivers\Drip as Drip;
 use Jeeglo\EmailService\Drivers\ConstantContact as ConstantContact;
 use Jeeglo\EmailService\Drivers\Aweber as Aweber;
 use Jeeglo\EmailService\Drivers\Kyvio as Kyvio;
+use Jeeglo\EmailService\Drivers\Ontraport as Ontraport;
+use Jeeglo\EmailService\Drivers\EmailOctopus as EmailOctopus;
 
 class EmailService {
 
@@ -65,6 +67,14 @@ class EmailService {
             case 'kyvio':
                 $this->driver = new Kyvio($credentials);
     			break;
+
+            case 'ontraport':
+                $this->driver = new Ontraport($credentials);
+                break;
+
+            case 'emailOctopus':
+                $this->driver = new EmailOctopus($credentials);
+                break;    
     		
     		default:
     			return 'Not Found';
