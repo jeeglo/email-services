fd464a92d8816675faf568d4eeaeee6695de5602
Rao Umair Ali <rao.kami98gmail.com>
2023-07-26 15:09:20 +0500

    Vbout integration completed

--- src/Drivers/Vbout.php
+++ src/Drivers/Vbout.php
@@ -2,15 +2,11 @@
 
 
 namespace Jeeglo\EmailService\Drivers;
-
 require_once dirname(dirname(__FILE__)) . '/VboutAPI/services/EmailMarketingWS.php';
-
 use EmailMarketingWS;
-
 
 class Vbout
 {
-
     protected $vboutAPI;
 
     public function __construct($credentials)
@@ -19,35 +15,37 @@ class Vbout
     }
 
     /**
+     * get lists form vbout
      * @return mixed
      */
     public function getLists()
     {
-
         try {
             $results = $this->vboutAPI->getMyLists();
-
             // Pass the $results array to the response() method to generate the formatted response
-            return $this->response($results['items']);
+            return $this->response($results);
         } catch (\Exception $e) {
             // Handle the exception if it occurs
-            // For example, you can log the error or rethrow the exception
             throw new \Exception("Error while getting lists: " . $e->getMessage());
         }
-
     }
 
-
-    private function response($lists)
+    /**
+     * Prepare response for list data
+     * @param $results
+     * @return array
+     * @throws \Exception
+     */
+    private function response($results)
     {
         $response = [];
 
         try {
-            if (!empty($lists)) {
-                foreach ($lists as $key => $list) {
+            if (isset($results['items']) && !empty($results['items'])) {
+                foreach ($results as $result) {
                     $response[] = array(
-                        'id' => $key,
-                        'name' => $list['name']
+                        'id' => $result['id'],
+                        'name' => $result['name']
                     );
                 }
             }
@@ -58,21 +56,21 @@ class Vbout
         return $response;
     }
 
+    /**
+     * Add contact of vbout
+     * @param $data
+     * @return array
+     */
     public function addContact($data)
     {
         $contact_data = array(
             'email' => $data['email'],
             'listid' => $data['list_id'],
-            'FIRSTNAME' => $data['first_name'],
-            'LASTNAME' => $data['last_name'],
             'status' => 'Active',
-            'fields' => array('first_name' => 'John')
-            );
+        );
 
-            $result = $this->vboutAPI->addNewContact($contact_data);
-            return $result;
-
+        $result = $this->vboutAPI->addNewContact($contact_data);
+        return $result;
     }
-
 
 }
