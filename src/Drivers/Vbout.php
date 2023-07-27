af7ec643d65e3a1b67a13073297b1edd5b6687a9
Rao Umair Ali <rao.kami98gmail.com>
2023-07-27 17:44:06 +0500

    Vbout integration completed with First Name, Last Name and Full Name.

--- src/Drivers/Vbout.php
+++ src/Drivers/Vbout.php
@@ -63,14 +63,46 @@ class Vbout
      */
     public function addContact($data)
     {
-        $contact_data = array(
-            'email' => $data['email'],
-            'listid' => $data['list_id'],
-            'status' => 'Active',
-        );
+        try {
+            $list_detail = $this->vboutAPI->getMyList($data['list_id']);
 
-        $result = $this->vboutAPI->addNewContact($contact_data);
-        return $result;
+            $first_name_id = null;
+            $last_name_id = null;
+            $full_name_id = null;
+
+            if (isset($list_detail['fields'])) {
+                $fields = $list_detail['fields'];
+                $first_name_id = array_search('First Name', $fields);
+                $last_name_id = array_search('Last Name', $fields);
+                $full_name_id = array_search('Full Name', $fields);
+            }
+
+            $fields_data = [];
+
+            if($first_name_id) {
+                $fields_data[$first_name_id] = $data['first_name'];
+            }
+
+            if($last_name_id) {
+                $fields_data[$last_name_id] = $data['last_name'];
+            }
+
+            if($full_name_id) {
+                $fields_data[$full_name_id] = $data['first_name'].' '.$data['last_name'];
+            }
+
+            $contact_data = array(
+                'email' => $data['email'],
+                'listid' => $data['list_id'],
+                'status' => 'Active',
+                'fields' => $fields_data
+            );
+
+            $result = $this->vboutAPI->addNewContact($contact_data);
+            return $result;
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+        }
     }
 
 }
